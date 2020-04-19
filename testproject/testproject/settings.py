42734fa6cf66f32572fce8c36e3bf2d90a0cb165
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-19 05:55:19 -0400

    Changes configurations in '/testproject/testproject/settings.py' back to previous configs

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(SECRET_KEY)
+SECRET_KEY = 'ligs+-e$2l46yy$g-(@^qrssy6xw4r-2ll#3e#(rfq*_ccd!#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['107.5.45.85', '127.0.0.1']
 
